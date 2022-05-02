6e0c7628a2778bd17a7abb135ca921d84958ff2a
Victor Saraev <vvsar@mail.ru>
2022-05-03 00:33:21 +0300

    feat: add stack solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,131 +1,46 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const { Node } = require('../extensions/list-tree.js');
+// const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-
-
 class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
 
   root() {
-    // throw new NotImplementedError('Not implemented');
-    return this.root.data;
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
-  add(data) {
-    // throw new NotImplementedError('Not implemented');
-    this.root = addWithin(this.root, data);
-    function addWithin(node, data) {
-      if(!node) {
-        return new Node(data);
-      }
-      if(node.data === data) {
-        return node;
-      }
-      if(data < node.data) {
-        node.left = addWithin(node.left, data);
-      } else {
-        node.right = addWithin(node.right, data);
-      }
-    }
+  add(/* data */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
-  has(data) {
-    // throw new NotImplementedError('Not implemented');
-    return searchWithin(this.root, data);
-    function searchWithin(node, data) {
-      if(!node) {
-        return false;
-      }
-      if(node.value === data) {
-        return true;
-      }
-      return data < node.data ? 
-        searchWithin(node.left, data) :
-        searchWithin(node.right, data);
-    }
+  has(/* data */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
-  find(data) {
-    // throw new NotImplementedError('Not implemented');
-    return findWithin(this.root, data);
-    function findWithin(node, data) {
-      if(!node) {
-        return;
-      }
-      if(node.value === data) {
-        return node.data;
-      }
-      return data < node.data ? 
-        findWithin(node.left, data) :
-        findWithin(node.right, data);
-    }
+  find(/* data */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
-  remove(data) {
-    // throw new NotImplementedError('Not implemented');
-    this.root = removeNode(this.root, data);
-    function removeNode(node, data) {
-      if (!node) {
-        return null;
-      }
-      if (data < node.data) {
-        node.left = removeNode(node.left, data);
-        return node;
-      } else if (data > node.data) {
-        node.right = removeNode(node.right, data);
-        return node;
-      } else {
-        if (!node.left && !node.right) {
-          return null;
-        }
-        if (!node.left) {
-          node = node.right;
-          return node;
-        }
-        if (!node.right) {
-          node = node.left;
-          return node;
-        }
-        let minFromRight = node.right;
-        while (minFromRight.left) {
-          minFromRight = minFromRight.left;
-        }
-        node.data = minFromRight.data;
-        node.right = removeNode(node.right, minFromRight.data);
-        return node;
-      }
-    }
+  remove(/* data */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
   min() {
-    // throw new NotImplementedError('Not implemented');
-    if (!this.root) {
-      return;
-    }
-    let node = this.root;
-    while(node.left) {
-      node = node.left;
-    }
-    return node.data;
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 
   max() {
-    // throw new NotImplementedError('Not implemented');
-    if (!this.root) {
-      return;
-    }
-    let node = this.root;
-    while(node.right) {
-      node = node.right;
-    }
-    return node.data;
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 }
 
--- src/stack.js
+++ src/stack.js
@@ -41,6 +41,3 @@ class Stack {
 module.exports = {
   Stack
 };
-
-
-
